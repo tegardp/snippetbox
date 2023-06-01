68cc1c1c36f82c95296706cc95e42f8843f8c7a5
Tegar Dani Pratama <tegar.dani@hukumonline.com>
2023-06-01 13:31:04 +0700

    🆕 feat(handlers.go): add handlers for home, snippetView, and snippetCreate ✨ feat(routes.go): add routes for home, snippetView, and snippetCreate 🆕 feat(helpers.go): add helper functions for serverError, clientError, and notFound 🆕 feat(main.go): add main function to start the server 🆕 feat(go.mod): add go module for dependency management 🆕 feat(go.sum): add go sum for dependency management 🆕 feat(snippet.go): add SnippetModel type and methods for inserting and retrieving snippets 🆕 feat(base.tmpl): add base template for HTML pages This commit adds the necessary code to create a basic web application that allows users to create and view snippets. The handlers for the home, snippetView, and snippetCreate pages are added, along with the routes for these pages. Helper functions for serverError, clientError, and notFound
    🎨 style(ui): add basic HTML and CSS files for home page
    This commit adds the basic HTML and CSS files for the home page. The HTML file contains a header, nav, and main section. The nav section contains a link to the home page. The CSS file contains basic styling for the header, nav, main, and footer sections. It also contains styling for buttons, forms, and snippets. Additionally, a JavaScript file is added to highlight the current page in the navigation bar.

---
+++ cmd/web/handlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Display specific snippet with ID %d...", id)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	// Pass the data to the SnippetModel.Insert() method, receiving the
+	// ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+}
